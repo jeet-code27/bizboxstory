d248789dd547c276844a36d64ac1207a00bad358
Your Name <jetendrasinghratore1@gmail.com>
2026-09-11 18:24:31 +0530

    Update hero section video with new YouTube short

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -170,7 +170,7 @@ export default function Hero() {
             >
               <iframe
                 className="absolute inset-0 w-full h-full"
-                src="https://www.youtube.com/embed/ksOiXHDHHpA?autoplay=1&mute=1&loop=1&playlist=ksOiXHDHHpA"
+                src="https://www.youtube.com/embed/5lwLH8CPto4?autoplay=1&mute=1&loop=1&playlist=5lwLH8CPto4"
                 title="BizBox Story Promo"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
